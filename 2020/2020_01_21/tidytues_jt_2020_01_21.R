1cfe1bc4575827a67e82990993525fcae4bb8133
jktruong <jktruong@bren.ucsb.edu>
2020-01-21 11:03:47 -0800

    Add data for Tuesday, Jan 21, 2020

---
+++ 2020/2020_01_21/tidytues_jt_2020_01_21.R
@@ -0,0 +1,7 @@
+# Attach packages
+
+library(tidyverse)
+
+# Read in TidyTuesday data
+
+spotify_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv')
