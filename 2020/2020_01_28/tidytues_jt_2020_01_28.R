24281dfd8e6e427f3ce30cf5dcfc0ec863a0568d
jktruong <jktruong@bren.ucsb.edu>
2020-01-28 11:37:57 -0800

    Add data for Tues Jan 28

---
+++ 2020/2020_01_28/tidytues_jt_2020_01_28.R
@@ -0,0 +1,7 @@
+# Attach packages
+
+library(tidyverse)
+
+# Read in data
+
+sf_trees <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-28/sf_trees.csv')
