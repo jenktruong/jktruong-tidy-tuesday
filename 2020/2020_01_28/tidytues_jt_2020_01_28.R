49f2492036661b8e1b67704513e99c81781458f1
jktruong <jktruong@bren.ucsb.edu>
2020-01-31 19:50:15 -0800

    Filter data frame to rows only containing 5 species

--- 2020/2020_01_28/tidytues_jt_2020_01_28.R
+++ 2020/2020_01_28/tidytues_jt_2020_01_28.R
@@ -33,6 +33,15 @@ sf_trees_top5 <- sf_trees_df %>%
   arrange(-n) %>%  # Arrange in descending order
   head(5) # Keep top 5 rows (has the most observations)
 
+# Go back to data frame and select only rows with those 5 species
+
+sf_trees_select <- sf_trees_df %>% 
+  filter(species %in% c("Lophostemon confertus :: Brisbane Box",
+                        "Tristaniopsis laurina :: Swamp Myrtle",
+                        "Arbutus 'Marina' :: Hybrid Strawberry Tree",
+                        "Platanus x hispanica :: Sycamore: London Plane",
+                        "Magnolia grandiflora :: Southern Magnolia"))
+
 # 2. Graph location
 
 # 3. Graph abuncance
