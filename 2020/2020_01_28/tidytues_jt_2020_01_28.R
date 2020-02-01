1fa3d5dc7ab0c0cab0c7743b48f5dd09bd305166
jktruong <jktruong@bren.ucsb.edu>
2020-01-31 19:33:35 -0800

    Redo whole script, create table for top 5 species count

--- 2020/2020_01_28/tidytues_jt_2020_01_28.R
+++ 2020/2020_01_28/tidytues_jt_2020_01_28.R
@@ -1,9 +1,38 @@
+# ----
+# Preliminary Stuff
+# ----
+
 # Attach packages
 
 library(tidyverse)
 library(here)
+library(pryr)
+library(visdat)
+library(skimr)
 library(lubridate)
+library(leaflet)
+library(janitor)
+library(tidyr)
 
 # Read in data
 
 sf_trees <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-28/sf_trees.csv')
+
+# Clean data
+
+sf_trees_df <- sf_trees %>%
+  drop_na()
+
+# ----
+# 1. Find 5 most common tree species in SF
+# ----
+
+sf_trees_top5 <- sf_trees_df %>% 
+  group_by(species) %>%  # Group by species
+  count() %>%  # Count occurences for each species, goes in ascending order
+  arrange(-n) %>%  # Arrange in descending order
+  head(5) # Keep top 5 rows (has the most observations)
+
+# 2. Graph location
+
+# 3. Graph abuncance
