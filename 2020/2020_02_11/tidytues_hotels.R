03c1f2cbb07d6b8b6010f2f52cced79dc8f0360e
jktruong <jktruong@bren.ucsb.edu>
2020-02-10 15:59:04 -0800

    Create R script for this week

---
+++ 2020/2020_02_11/tidytues_hotels.R
@@ -0,0 +1,17 @@
+############
+# Tidy Tuesday Week 6 2020: Hotel Bookings
+# February 11, 2020
+# Jennifer Truong
+############
+
+# ---
+# Preliminary Stuff
+# ---
+
+# Attach packages
+
+library(tidyverse)
+
+# Read in data
+
+hotels <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-11/hotels.csv')
