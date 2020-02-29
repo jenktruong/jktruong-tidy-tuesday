4fe876a865b4760e748af16b67f8dba89b9a61db
jktruong <jktruong@bren.ucsb.edu>
2020-02-28 21:53:36 -0800

    Wrote comment about what I want to do with data

--- 2020/2020_02_25/tidytues_measles.R
+++ 2020/2020_02_25/tidytues_measles.R
@@ -11,3 +11,7 @@ library(tidyverse)
 # 2. Read in data -------------------
 
 measles <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-25/measles.csv')
+
+# 3. Wrangle data -------------------
+
+# Find average MMR rate and total enrollment by state
