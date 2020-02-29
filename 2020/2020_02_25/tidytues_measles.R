52012f72343d33ae8db2252ddab8ceb10cbb1256
jktruong <jktruong@bren.ucsb.edu>
2020-02-28 20:50:41 -0800

    Create R script for Feb 25 2020

---
+++ 2020/2020_02_25/tidytues_measles.R
@@ -0,0 +1,13 @@
+############
+# Tidy Tuesday Week 9 2020: Food Consumption and CO2 emissions
+# February 25, 2020
+# Jennifer Truong
+############
+
+# 1. Attach packages ----------------
+
+library(tidyverse)
+
+# 2. Read in data -------------------
+
+measles <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-25/measles.csv')
