ff2a79306c6030515e1e5fdcd437b7994607b86e
jktruong <jktruong@bren.ucsb.edu>
2020-03-03 11:03:27 -0800

    Create R script for Tidy Tuesday NHL script

---
+++ 2020/2020_03_03/tidytues_nhlhockey.R
@@ -0,0 +1,17 @@
+############
+# Tidy Tuesday Week 10 2020: NHL Hockey Goals
+# March 3, 2020
+# Jennifer Truong
+############
+
+# 1. Attach packages --------
+
+library(tidyverse)
+
+# 2. Read in data -----------
+
+game_goals <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-03/game_goals.csv')
+
+top_250 <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-03/top_250.csv')
+
+season_goals <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-03/season_goals.csv')
