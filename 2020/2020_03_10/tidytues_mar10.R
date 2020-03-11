2f6880ad9283ec55fd7eddb96a517ac4025e4d4a
jktruong <jktruong@bren.ucsb.edu>
2020-03-11 10:35:15 -0700

    Read in data

---
+++ 2020/2020_03_10/tidytues_mar10.R
@@ -0,0 +1,21 @@
+############
+# Tidy Tuesday Week 11 2020: Food Consumption and CO2 emissions
+# March 10, 2020
+# Jennifer Truong
+############
+
+# Attach packages ----
+
+library(tidyverse)
+
+# Read in data ----
+
+tuition_cost <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/tuition_cost.csv')
+
+tuition_income <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/tuition_income.csv') 
+
+salary_potential <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/salary_potential.csv')
+
+historical_tuition <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/historical_tuition.csv')
+
+diversity_school <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-10/diversity_school.csv')
