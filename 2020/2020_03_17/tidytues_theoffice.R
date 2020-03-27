9d2d0f687263de95e6cf2caa7c9d90ca973a9561
jktruong <jktruong@bren.ucsb.edu>
2020-03-26 17:53:29 -0700

    Create scripts for Tidy Tuesday sessions that I missed

---
+++ 2020/2020_03_17/tidytues_theoffice.R
@@ -0,0 +1,7 @@
+# Attach tidyverse
+
+library(tidyverse)
+
+# Read in data
+
+office_ratings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-17/office_ratings.csv')
---
+++ 2020/2020_03_24/tidytues_tbi.R
@@ -0,0 +1,11 @@
+# Attach tidyverse
+
+library(tidyverse)
+
+# Read in data
+
+
+tbi_age <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-24/tbi_age.csv')
+tbi_year <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-24/tbi_year.csv')
+tbi_military <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-24/tbi_military.csv')
+
